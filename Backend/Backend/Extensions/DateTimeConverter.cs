1cda126a16530ef27f6cda541b319e392f8ed75a
Martin Zoltán Sillye <mz.sillye@edu.bme.hu>
2022-12-01 00:35:17 +0100

    fix datetime format

---
+++ Backend/Backend/Extensions/DateTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+
+namespace Backend.Extensions
+{
+    public class DateTimeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            var dateStr = ((DateTime)value!).ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz", CultureInfo.InvariantCulture);
+            JToken t = JToken.FromObject(dateStr);
+            t.WriteTo(writer);
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsAssignableFrom(typeof(DateTime));
+        }
+
+        public override bool CanRead { get; } = false;
+    }
+}
